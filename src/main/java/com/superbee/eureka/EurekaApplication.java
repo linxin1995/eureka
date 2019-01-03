91e9e5a482089c9f27453c73422aae73472dc8b6
林新 <1320274021@qq.com>
2019-01-03 16:41:44 +0800

    eureka更新配置解决之前的一些不足

--- src/main/java/com/superbee/eureka/EurekaApplication.java
+++ src/main/java/com/superbee/eureka/EurekaApplication.java
@@ -1,12 +1,16 @@
-package cc.linxin.cloud.eureka;
+package com.superbee.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaApplication.class, args);
     }
+
 }
+
